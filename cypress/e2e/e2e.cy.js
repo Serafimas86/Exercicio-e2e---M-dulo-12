a1bbb394e178943c18abe01b541a51d71d4b5051
aline <aline.serafim86@gmail.com>
2022-09-16 14:52:48 -0300

    Exercicio de teste costumizado

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,33 +1,21 @@
 /// <reference types="cypress" />
 
+const perfil = require('../fixtures/perfil.json')
+
 describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
     beforeEach(() => {
         cy.visit('produtos')
+
     });
 
     //Selecionar Produto e Adicionar ao carrinho
     it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get(':nth-child(3) > .page-numbers').click()
-        cy.addProdutos('Arcadio Gym Short', '32', 'Blue' , 1)
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 1)
-
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get(':nth-child(3) > .page-numbers').click()
-        cy.addProdutos('Atomic Endurance Running Tee (Crew-Neck)', 'XS', 'Black' , 1)
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
-
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get(':nth-child(3) > .page-numbers').click() 
-        cy.addProdutos('Ajax Full-Zip Sweatshirt', 'XS', 'Red' , 1)
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 3)
-
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get(':nth-child(3) > .page-numbers').click()
-        cy.addProdutos('Augusta Pullover Jacket', 'XS', 'Orange' , 1)
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 4)
-
+        cy.addProdutos('Celeste Sports Bra', 'XS', 'Green' , 1)  
+        cy.addProdutos('Chaz Kangeroo Hoodie', 'XS', 'Black' , 1)
+        cy.addProdutos('Circe Hooded Ice Fleece', 'XS', 'Gray' , 1)
+        cy.addProdutos('Deion Long-Sleeve EverCool™ Tee', 'XS', 'White' , 1)
+        
     //Dados Cadastrais
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
@@ -41,7 +29,7 @@ describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#terms').click({force:true })
         cy.get('#place_order').click({force:true })
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
-
+    
     });
-
-})
+    
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,12 +32,14 @@ Cypress.Commands.add('login', (usuario, senha) => {
 });
 
 Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) => {
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get(':nth-child(3) > .page-numbers').click()
     cy.get('[class="product-block grid"]')
        .contains(produto).click()
     cy.get('.button-variable-item-'+ tamanho).click()
     cy.get('.button-variable-item-'+ cor).click()
     cy.get('.input-text').clear().type(quantidade)
     cy.get('.single_add_to_cart_button').click()
- })
+ });
 
 
