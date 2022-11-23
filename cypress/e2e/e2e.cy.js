6f28b4376c76a250d1b800751ce4b972a69c6e51
aline <aline.serafim86@gmail.com>
2022-11-23 17:17:09 -0300

    Alteracao de produtos

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -10,11 +10,11 @@ describe('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     });
 
     //Selecionar Produto e Adicionar ao carrinho
-    it('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
-        cy.addProdutos('Celeste Sports Bra', 'XS', 'Green' , 1)  
-        cy.addProdutos('Chaz Kangeroo Hoodie', 'XS', 'Black' , 1)
-        cy.addProdutos('Circe Hooded Ice Fleece', 'XS', 'Gray' , 1)
-        cy.addProdutos('Deion Long-Sleeve EverCool™ Tee', 'XS', 'White' , 1)
+    it.only('Deve adicionar produtos ao carrinho - Usando comando customizado', () => {
+        cy.addProdutos('Balboa Persistence Tee', 'XS', 'Gray' , 1)  
+        cy.addProdutos('Beaumont Summit Kit', 'XS', 'Orange' , 1)
+        cy.addProdutos('Bruno Compete Hoodie', 'XS', 'Black' , 1)
+        cy.addProdutos('Caesar Warm-Up Pant', '32', 'Black' , 1)
         
     //Dados Cadastrais
         cy.get('.woocommerce-message > .button').click()
