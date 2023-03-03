7040d66bce5cc84654c36425391785d539bd0dff
aline <aline.serafim86@gmail.com>
2023-03-02 22:34:57 -0300

    Acréscimo de timeout

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 10000,
   e2e: {
    
     setupNodeEvents(on, config) {
